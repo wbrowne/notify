cd892d218db08797892c8aa2d7d7caaae9ce7ad8
wbrowne <wbrowne@tcd.ie>
2020-05-08 17:34:59 +0200

    reformat

--- main.rb
+++ main.rb
@@ -13,22 +13,18 @@ end
 
 def changes? (b)
   url = 'https://doodle.com/poll/9tvdktysbrps8p9z'
-
-  puts '-' * 80
-  log 'Trying again'
   b.goto url
+
   log 'Page has loaded'
 
   selector = '#d-pollView article div div div ul li:nth-child(1) label div.d-optionDetails div button.d-button.d-countButton.d-medium.d-silentButton div div.d-buttonContent div.d-textContainer div'
-
   tag = b.element css: selector
-
   participants = tag.text
 
   if participants != '32/32'
     notify 'A slot is available!'
 
-    log 'Enter y to continue or anything else to quit.'
+    log 'Enter \'y\' to continue or anything else to quit.'
     return gets.chomp.downcase != 'y'
   else
     log 'No luck this time.'
